83f57f037dd15b3ccd1077eef84d06fc427ea87a
Femurbreak <flynngoold@gmail.com>
2024-02-17 16:39:53 +1100

    Timer and Start Game
    Adds the following:
    - A timer
    - A start game button
    Allowing for the player to view their points at the end of 10 seconds

---
+++ GameManager.gd
@@ -0,0 +1,47 @@
+extends Node
+
+var points = 0
+var time = 0
+var timer_on = false
+
+@onready var points_label = %PointsLabel
+@onready var GarbageCan = %Garbage_Can
+@onready var Timer_Label = %TimerLabel
+
+var garbageBag = preload("res://Scenes/garbage_bag.tscn")
+
+func add_point():
+	if(timer_on == true):
+		points += 1
+		print(points)
+		points_label.text = "Points: " + str(points) 
+
+func instGarbageBag():
+	var instance = garbageBag.instantiate()
+	instance.position = Vector2(-448,274)
+	add_child(instance)
+
+func _process(delta):
+	if(timer_on == true):
+		time += delta
+		print(time)
+		Timer_Label.text = "Time: " + str(time)
+	if(time >= 10):
+		timer_on = false
+
+func _on_button_pressed():
+	timer_on = true
+	points = 0
+	time = 0
+
+
+
+#func _ready():
+	# instGarbageBag()
+	# add_point()
+	# GarbageCan.game_manager = self
+
+
+
+
+
